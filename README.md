9072a895a5235d13f0d979e1bfa66d7fc98d21e6
jaredboese <j_boese@ga-tech.co.jp>
2024-07-23 09:51:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# PDF Watermark Tool
+
+This tool allows you to add a watermark to PDF files using a custom font.
+
+## Prerequisites
+
+- Python 3.x
+- `pip`  (Python package installer, pip3 also fine)
+
+## Installation
+
+1. After cloning, Install the required dependencies:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage (If you want to run the script from terminal)
+
+1. Run the script:
+
+    ```bash
+    python pdf.py
+    ```
+
+2. Select the PDF files you want to watermark. (Can Select multipe files via shift+click)
+3. Choose the output file name and location.
+
+## Building an Executable with PyInstaller (If you want to have it as an executable)
+
+To create a standalone executable:
+
+1. Run PyInstaller with the spec file:
+
+    ```bash
+    pyinstaller pdf.spec
+    ```
+
+2. The executable will be created in the `dist` directory.
---
+++ pdf.py
@@ -0,0 +1,62 @@
+from PyPDF2 import PdfReader, PdfWriter
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
+import tkinter as tk
+from tkinter import filedialog
+import io
+import os
+
+basedir = os.path.dirname(__file__)
+
+def create_watermark(text):
+    packet = io.BytesIO()
+    can = canvas.Canvas(packet, pagesize=letter)
+    font_path = os.path.join(basedir, 'ipaexg.ttf')
+    pdfmetrics.registerFont(TTFont('IPAexGothic', font_path))
+    can.setFont('IPAexGothic', 12)  
+    can.drawString(400, 50, text)  # 一位設定する
+    can.save()
+    packet.seek(0)
+    return PdfReader(packet)
+
+def merge_pdfs(pdf_list, output, watermark_text):
+    pdf_writer = PdfWriter()
+    watermark = create_watermark(watermark_text)
+
+    for pdf in pdf_list:
+        pdf_reader = PdfReader(pdf)
+        num_pages = len(pdf_reader.pages)
+
+        for page_num in range(num_pages):
+            page = pdf_reader.pages[page_num]
+            if watermark_text:
+                page.merge_page(watermark.pages[0])
+            pdf_writer.add_page(page)
+
+    with open(output, 'wb') as out:
+        pdf_writer.write(out)
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.withdraw()
+
+    pdf_files = filedialog.askopenfilenames(
+        title="PDFファイルを選択してください",
+        filetypes=(("PDF files", "*.pdf"),)
+    )
+
+    if not pdf_files:
+        print("ファイル選択できませんでした。")
+        exit()
+
+    output_pdf = filedialog.asksaveasfilename(
+        title="保存先を選択してください",
+        filetypes=(("PDF files", "*.pdf"),),
+        defaultextension=".pdf"
+    )
+
+    watermark_text = '株式会社GA Technologies'  
+
+    merge_pdfs(pdf_files, output_pdf, watermark_text)
